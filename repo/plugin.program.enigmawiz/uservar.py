e072567395ceb3cc671fce714ef45546ed0ace71
Nate <n.roberts1999@hotmail.com>
2023-07-12 16:54:27 +0100

    edit

--- repo/plugin.program.enigmawiz/uservar.py
+++ repo/plugin.program.enigmawiz/uservar.py
@@ -1,7 +1,7 @@
 import xbmcaddon
 
 '''#####-----Build File-----#####'''
-buildfile = 'http://team-enigma.xyz/builds.xml'
+buildfile = 'https://team-enigma.xyz/builds.xml'
 
 '''#####-----Notifications File-----#####'''
 notify_url  = 'http://'
