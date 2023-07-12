806251967b2772933b48dad04a4432ddfe3faf30
Nate <n.roberts1999@hotmail.com>
2023-07-12 16:11:47 +0100

    edit

---
+++ repo/zips/plugin.program.enigmawiz/plugin.program.enigmawiz/uservar.py
@@ -0,0 +1,10 @@
+import xbmcaddon
+
+'''#####-----Build File-----#####'''
+buildfile = 'https://github.com/JxrJxrBlinkz/jxrjxrblinkz.github.io/blob/master/builds.xml'
+
+'''#####-----Notifications File-----#####'''
+notify_url  = 'https://github.com/JxrJxrBlinkz/jxrjxrblinkz.github.io/blob/master/notify.txt'
+
+'''#####-----Excludes-----#####'''
+excludes  = ['plugin.video.whatever']
