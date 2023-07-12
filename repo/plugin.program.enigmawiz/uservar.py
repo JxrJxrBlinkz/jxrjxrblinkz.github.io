f69199ed50231a706b1ff053ce17169ae3459421
Nate <n.roberts1999@hotmail.com>
2023-07-12 15:53:04 +0100

    Update uservar.py

--- repo/plugin.program.enigmawiz/uservar.py
+++ repo/plugin.program.enigmawiz/uservar.py
@@ -1,7 +1,7 @@
 import xbmcaddon
 
 '''#####-----Build File-----#####'''
-buildfile = 'https://github.com/JxrJxrBlinkz/jxrjxrblinkz.github.io/blob/master/builds.txt'
+buildfile = 'https://github.com/JxrJxrBlinkz/jxrjxrblinkz.github.io/blob/master/builds.xml'
 
 '''#####-----Notifications File-----#####'''
 notify_url  = 'https://github.com/JxrJxrBlinkz/jxrjxrblinkz.github.io/blob/master/notify.txt'
